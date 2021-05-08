8277ff5822f89573412fac69ec38746052218c60
AREEG94FAHAD <fahedareeg@gmail.com>
2021-05-09 02:10:47 +0300

    Update today.jsx

--- weather-app/src/components/today.jsx
+++ weather-app/src/components/today.jsx
@@ -1,87 +1,131 @@
-import React from "react";
-import "./today.css";
-import sn from "./wallpapers/snow.svg";
-import sl from "./wallpapers/sleet.svg";
-import h from "./wallpapers/hail.svg";
-import t from "./wallpapers/thunderstorm.svg";
-import hr from "./wallpapers/heavyrain.svg";
-import lr from "./wallpapers/lightrain.svg";
-import s from "./wallpapers/showers.svg";
-import hc from "./wallpapers/heavycloud.svg";
-import lc from "./wallpapers/lightcloud.svg";
-import c from "./wallpapers/clear.svg";
+import React from "react"
+import Search from "./search"
+import "./today.css"
+import sn from "./wallpapers/snow.svg"
+import sl from "./wallpapers/sleet.svg"
+import h from "./wallpapers/hail.svg"
+import t from "./wallpapers/thunderstorm.svg"
+import hr from "./wallpapers/heavyrain.svg"
+import lr from "./wallpapers/lightrain.svg"
+import s from "./wallpapers/showers.svg"
+import hc from "./wallpapers/heavycloud.svg"
+import lc from "./wallpapers/lightcloud.svg"
+import c from "./wallpapers/clear.svg"
 
 export default function today(props) {
-    const backgroundImage = [sn, sl, h, t, h, hr, lr, s, hc, lc, c];
-    const backgroundImageIndex = [
-        "sn",
-        "sl",
-        "h",
-        "t",
-        "h",
-        "hr",
-        "lr",
-        "s",
-        "hc",
-        "lc",
-        "c",
-    ];
-    function convertTime(date) {
-        const event = new Date(date);
-        return event.toLocaleTimeString("en-US");
-    }
-    return (
-        <div>
-            <div
-                style={{
+  let imgurl = ""
+  const backgroundImage = [
+    {
+      name: "snow",
+      url: sn,
+    },
+    {
+      name: "sleet",
+      url: sl,
+    },
+    {
+      name: "hail",
+      url: h,
+    },
+    {
+      name: "thunderstorm",
+      url: t,
+    },
+    {
+      name: "heavy rain",
+      url: hr,
+    },
+    {
+      name: "light rain",
+      url: lr,
+    },
+    {
+      name: "shower rain",
+      url: s,
+    },
+    {
+      name: "rain",
+      url: lr,
+    },
+    {
+      name: "broken clouds",
+      url: hc,
+    },
+    {
+      name: "few clouds",
+      url: lc,
+    },
+    {
+      name: "overcast clouds",
+      url: hc,
+    },
+    {
+      name: "scattered clouds",
+      url: lc,
+    },
+    {
+      name: "clear",
+      url: c,
+    },
+  ]
+  if (props.state.data.weather) {
+    backgroundImage.map((item) =>
+      props.state.data.weather[0].description.includes(item.name)
+        ? (imgurl = item.url)
+        : null
+    )
+  }
 
-                    display: "flex",
-                    flexWrap: "wrap",
-                    justifyContent: "space-around",
-                }}
-            >
-                <img style={{
-                    width: "130px"
-                }} src={backgroundImage[backgroundImageIndex.indexOf(props.state.data.weather_state_abbr)]} alt="" />
+  function convertTime(date) {
+    const event = new Date(date * 1000)
+    return event.toLocaleTimeString()
+  }
+  return (
+    <div>
+      <div
+        style={{
+          display: "flex",
+          flexWrap: "wrap",
+          justifyContent: "space-around",
+        }}
+      >
+        <div style={{ marginBottom: "20px" }} className="right-panel panel">
+          <Search onClick={props.onClick} />
+          <div>
+            <h4>
+              {props.state.data.weather
+                ? props.state.data.weather[0].description
+                : null}
+            </h4>{" "}
+          </div>
 
-                <div className="left-panel panel">
-                    <div className="date">
-                        {props.state.date[0]} {props.state.data.applicable_date}
-                        <br />
-                    </div>
-                    <div className="city">{props.state.title}</div>
-                    <div className="temp">{Math.floor(props.state.the_temp[0])}&deg;</div>
-                </div>
-                <div className="right-panel panel">
-                    <div className='search'>
-                        <ul id="growing-search-freebie">
-                            <li>
-                                <div className="growing-search">
-                                    <div className="input">
-                                        <input onChange={props.onChange} type="text" name="search" />
-                                    </div><div className="submit">
-                                        <button onClick={props.onClick} type="submit" name="go_search">
-                                            <span className="fa fa-search"></span>
-                                        </button>
-                                    </div>
-                                </div>
-                            </li>
-                        </ul>
-                    </div>
-                    <div>
-                        <h4>{props.state.data.weather_state_name}</h4>{" "}
-                    </div>
-
-                    <div>
-                        Sun rise {convertTime(props.state.sun_rise)}{" "}
-                        <i className="fas fa-sun"></i>
-                    </div>
-                    <div>
-                        Sun set {convertTime(props.state.sun_set)}{" "}
-                        <i className="fas fa-moon"></i>
-                    </div>
-                </div>
-            </div>
+          <div>
+            Sun rise {convertTime(props.state.sun_rise)}{" "}
+            <i className="fas fa-sun"></i>
+          </div>
+          <div>
+            Sun set {convertTime(props.state.sun_set)}{" "}
+            <i className="fas fa-moon"></i>
+          </div>
         </div>
-    );
+
+        <div className="left-panel panel">
+          <div className="date">
+            {props.state.date[0]}
+            <br />
+          </div>
+          <div className="city">{props.state.title}</div>
+          <div className="temp">{Math.floor(props.state.the_temp[0])}&deg;</div>
+        </div>
+        <img
+          style={{
+            width: "130px",
+            height: "intrinsic",
+          }}
+          src={imgurl}
+          alt=""
+        />
+      </div>
+    </div>
+  )
 }
